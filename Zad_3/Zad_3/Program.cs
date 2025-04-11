ba22b017057718d0770abd5cc531a8a91b1c97bf
eryk623 <e.widlarz783@gmail.com>
2025-04-11 20:17:30 +0200

    Add files via upload

---
+++ Zad_3/Zad_3/Program.cs
@@ -0,0 +1,213 @@
+﻿using System.Globalization;
+
+namespace AlgorytmGenetyczny
+{
+    class Specimen
+    {
+        public static int NumOfPar, LBnP;
+        public static int chromLen => NumOfPar * LBnP;
+        public static double minVal, maxVal;
+        public string specimen { get; private set; }
+        public List<string> xChrom { get; private set; }
+        public List<double> xVal { get; private set; }
+        public double? rate { get; set; }
+
+        public Specimen(string specimen)
+        {
+            this.specimen = specimen;
+            cut();
+            decode(xChrom);
+            rate = null;
+        }
+
+        private void cut()
+        {
+            List<string> xChrom = new List<string>();
+            for (int i = 0; i < NumOfPar; i++)
+            {
+                xChrom.Add(specimen.Substring(i * LBnP, LBnP));
+            }
+
+            this.xChrom = xChrom;
+        }
+
+        private void decode(List<string> xChrom)
+        {
+            double ZD = maxVal - minVal;
+            List<double> xVal = new List<double>();
+            for (int i = 0; i < NumOfPar; i++)
+            {
+                if (xChrom[i].All(chrom => chrom == '0'))
+                {
+                    xVal.Add(minVal);
+                }
+                else if (xChrom[i].All(chrom => chrom == '1'))
+                {
+                    xVal.Add(maxVal);
+                }
+                else
+                {
+                    double ctmp = 0;
+                    string xRev = new string(xChrom[i].Reverse().ToArray());
+                    for (int j = 0; j < LBnP; j++)
+                    {
+                        if (xRev[j] != '0')
+                        {
+                            ctmp += Math.Pow(2, j);
+                        }
+                    }
+                    xVal.Add(Math.Round(minVal + ctmp / (Math.Pow(2, LBnP) - 1) * ZD, 2));
+                }
+            }
+            this.xVal = xVal;
+        }
+
+        public void showSpecimen()
+        {
+            Console.Write($"{specimen}  |  ");
+            
+            foreach (var val in xChrom)
+            {
+                Console.Write($"{val} ");
+            }
+            Console.Write(" | ");
+
+            foreach (var val in xVal)
+            {
+                Console.Write($"{val,6:F2} ");
+            }
+            Console.Write($"  rate: {rate:F2} ");
+            Console.WriteLine();
+        }
+    }
+
+    class Program
+    {
+        static List<Specimen> initializePopulation(int specimensCount, int specimenLen)
+        {
+            List<Specimen> population = new List<Specimen>();
+            Random random = new Random();
+
+            for (int i = 0; i < specimensCount; i++)
+            {
+                string newSpecimen = "";
+                for (int j = 0; j < specimenLen; j++)
+                {
+                    newSpecimen += Convert.ToString(random.Next(0, 2));
+                }
+                population.Add(new Specimen(newSpecimen));
+            }
+            return population;
+        }
+
+        static void showPopulation(List<Specimen> population)
+        {
+            foreach (var specimen in population)
+            {
+                specimen.showSpecimen();
+            }
+        }
+        
+        static void evaluatePopulation(List<Specimen> population, List<(int x1, int x2, int x3)> probes)
+        {
+            foreach (var s in population)
+            {
+                double rate = 0;
+                foreach (var p in probes)
+                {
+                    double neuron1 = 1 / (1 + Math.Exp(-(p.x1 * s.xVal[0] + p.x2 * s.xVal[1] + s.xVal[2])));
+                    double neuron2 = 1 / (1 + Math.Exp(-(p.x1 * s.xVal[3] + p.x2 * s.xVal[4] + s.xVal[5])));
+                    double output = 1 / (1 + Math.Exp(-(neuron1 * s.xVal[6] + neuron2 * s.xVal[7] + s.xVal[8])));
+                    rate += Math.Pow(p.x3 - output, 2);
+                }
+                s.rate = rate;
+            }
+        }
+        
+        static List<Specimen> tournamentSelection(List<Specimen> population)
+        {
+            List<Specimen> selected = new List<Specimen>();
+            Random random = new Random();
+
+            for (int i = 0; i < population.Count - 1; i++)
+            {
+                Specimen choice1 = population[random.Next(0, population.Count)];
+                Specimen choice2 = population[random.Next(0, population.Count)];
+                Specimen choice3 = population[random.Next(0, population.Count)];
+                List<Specimen> choices = [choice1, choice2, choice3];
+                selected.Add(choices.MinBy(choice => choice.rate));
+            }
+            return selected;
+        }
+        
+        static void mutator(List<Specimen> newPopulation)
+        {
+            Random random = new Random();
+
+            for (int i = 4; i < newPopulation.Count - 1; i++)
+            {
+                int bPoint = random.Next(0, Specimen.chromLen);
+                char[] chrom = newPopulation[i].specimen.ToCharArray();
+                chrom[bPoint] = chrom[bPoint] == '0' ? '1' : '0';
+                newPopulation[i] = new Specimen(new string(chrom));
+            }
+        }
+
+        static void hotDeck(List<Specimen> newPopulation, List<Specimen> population)
+        {
+            newPopulation.Add(population.MinBy(s => s.rate));
+        }
+
+        static void hybridization(List<Specimen> newPopulation)
+        {
+            Random random = new Random();
+            for (int i = 0; i < newPopulation.Count - 1; i += 2)
+            {
+                if (i > 3 && i <= 7) continue;
+                int cut = random.Next(1, Specimen.chromLen - 1);
+                (string, string) p1 = (newPopulation[i].specimen.Substring(0, cut), newPopulation[i].specimen.Substring(cut));
+                (string, string) p2 = (newPopulation[i + 1].specimen.Substring(0, cut), newPopulation[i + 1].specimen.Substring(cut));
+                newPopulation[i] = new Specimen(p1.Item1 + p2.Item2);
+                newPopulation[i + 1] = new Specimen(p2.Item1 + p1.Item2);
+            }
+        }
+
+        static void geneticAlghoritm(int iterationCount, int specimenCount)
+        {
+            List<(int we1, int we2, int d)> probes = [(0, 0, 0), (0, 1, 1), (1, 0, 1), (1, 1, 0)];
+            List<Specimen> population = initializePopulation(specimenCount, Specimen.chromLen);
+            evaluatePopulation(population, probes);
+            Specimen theBest = population.MinBy(s => s.rate);
+            double populationAverage = Math.Round((double)population.Average(s => s.rate), 2);
+            Console.WriteLine("Pierwsza populacja");
+            showPopulation(population);
+            Console.WriteLine($"Najlepszy z populacji:  {theBest.specimen} | {theBest.rate:F2}\nŚrednia populacji: {populationAverage}\n");
+
+            for (int i = 0; i < iterationCount; i++)
+            {
+                List<Specimen> newPopulation = tournamentSelection(population);
+                hybridization(newPopulation);
+                mutator(newPopulation);
+                evaluatePopulation(newPopulation, probes);
+                hotDeck(newPopulation, population);
+                theBest = newPopulation.MinBy(s => s.rate);
+                populationAverage = Math.Round((double)newPopulation.Average(s => s.rate), 2);
+                Console.WriteLine($"Iteracja numer: {i + 1}");
+                showPopulation(newPopulation);
+                Console.WriteLine($"Najlepszy z populacji:  {theBest.specimen} | {theBest.rate:F2}\nŚrednia populacji: {populationAverage}\n");
+                population = newPopulation;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int specimenCount = 13;
+            int iterationCount = 200;
+            Specimen.LBnP = 4;
+            Specimen.NumOfPar = 9;
+            Specimen.minVal = -10;
+            Specimen.maxVal = 10;
+            geneticAlghoritm(iterationCount, specimenCount);
+        }
+    }
+}
