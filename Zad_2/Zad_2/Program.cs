1fc44dcabaf73250f3ef978cb95dee02d6293368
eryk623 <e.widlarz783@gmail.com>
2025-04-11 18:41:45 +0200

    Add files via upload

--- Zad_2/Zad_2/Program.cs
+++ Zad_2/Zad_2/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Globalization;
+﻿using System.Globalization;
 
 namespace AlgorytmGenetyczny
 {
@@ -12,7 +11,7 @@ namespace AlgorytmGenetyczny
         public List<string> xChrom { get; private set; }
         public List<double> xVal { get; private set; }
         public double? rate { get; set; }
-        
+
         public Specimen(string specimen)
         {
             this.specimen = specimen;
@@ -28,6 +27,7 @@ namespace AlgorytmGenetyczny
             {
                 xChrom.Add(specimen.Substring(i * LBnP, LBnP));
             }
+
             this.xChrom = xChrom;
         }
 
@@ -37,8 +37,14 @@ namespace AlgorytmGenetyczny
             List<double> xVal = new List<double>();
             for (int i = 0; i < NumOfPar; i++)
             {
-                if (xChrom[i].All(chrom => chrom == '0')) {xVal.Add(minVal);}
-                else if (xChrom[i].All(chrom => chrom == '1')) {xVal.Add(maxVal);}
+                if (xChrom[i].All(chrom => chrom == '0'))
+                {
+                    xVal.Add(minVal);
+                }
+                else if (xChrom[i].All(chrom => chrom == '1'))
+                {
+                    xVal.Add(maxVal);
+                }
                 else
                 {
                     double ctmp = 0;
@@ -50,33 +56,31 @@ namespace AlgorytmGenetyczny
                             ctmp += Math.Pow(2, j);
                         }
                     }
-                    xVal.Add(Math.Round(minVal + ctmp/(Math.Pow(2, LBnP) - 1) * ZD, 2));
+                    xVal.Add(Math.Round(minVal + ctmp / (Math.Pow(2, LBnP) - 1) * ZD, 2));
                 }
             }
             this.xVal = xVal;
         }
-        
+
         public void showSpecimen()
         {
             Console.Write($"{specimen}  |  ");
+            
             foreach (var val in xChrom)
             {
                 Console.Write($"{val} ");
             }
-
             Console.Write(" | ");
 
             foreach (var val in xVal)
             {
                 Console.Write($"{val,5:F2} ");
             }
-            
             Console.Write($"  rate: {rate:F2} ");
             Console.WriteLine();
-            
         }
     }
-    
+
     class Program
     {
         static List<Specimen> initializePopulation(int specimensCount, int specimenLen)
@@ -95,7 +99,7 @@ namespace AlgorytmGenetyczny
             }
             return population;
         }
-        
+
         static void showPopulation(List<Specimen> population)
         {
             foreach (var specimen in population)
@@ -121,7 +125,7 @@ namespace AlgorytmGenetyczny
         {
             List<Specimen> selected = new List<Specimen>();
             Random random = new Random();
-            
+
             for (int i = 0; i < population.Count - 1; i++)
             {
                 Specimen choice1 = population[random.Next(0, population.Count)];
@@ -132,22 +136,7 @@ namespace AlgorytmGenetyczny
             }
             return selected;
         }
-
-        static List<Specimen> mutatorr(List<Specimen> newPopulation)
-        {
-            List<Specimen> mutated = new List<Specimen>();
-            Random random = new Random();
-
-            foreach (var specimen in newPopulation)
-            {
-                int bPoint = random.Next(0, specimen.specimen.Length);
-                char[] chrom = specimen.specimen.ToCharArray();
-                chrom[bPoint] = chrom[bPoint] == '0' ? '1' : '0';
-                mutated.Add(new Specimen(new string(chrom)));
-            }
-            return mutated;
-        }
-
+        
         static void mutator(List<Specimen> newPopulation)
         {
             Random random = new Random();
@@ -157,7 +146,7 @@ namespace AlgorytmGenetyczny
                 int bPoint = random.Next(0, Specimen.chromLen);
                 char[] chrom = newPopulation[i].specimen.ToCharArray();
                 chrom[bPoint] = chrom[bPoint] == '0' ? '1' : '0';
-                newPopulation[i] =  new Specimen(new string(chrom));
+                newPopulation[i] = new Specimen(new string(chrom));
             }
         }
 
@@ -172,9 +161,9 @@ namespace AlgorytmGenetyczny
             for (int i = 0; i < newPopulation.Count - 1; i += 2)
             {
                 if (i > 3 && i <= 7) continue;
-                int cut = random.Next(0, Specimen.chromLen - 2);
-                (string, string) p1 = (newPopulation[i].specimen.Substring(0, cut + 1), newPopulation[i].specimen.Substring(cut + 1));
-                (string, string) p2 = (newPopulation[i + 1].specimen.Substring(0, cut + 1), newPopulation[i + 1].specimen.Substring(cut + 1));
+                int cut = random.Next(1, Specimen.chromLen - 1);
+                (string, string) p1 = (newPopulation[i].specimen.Substring(0, cut), newPopulation[i].specimen.Substring(cut));
+                (string, string) p2 = (newPopulation[i + 1].specimen.Substring(0, cut), newPopulation[i + 1].specimen.Substring(cut));
                 newPopulation[i] = new Specimen(p1.Item1 + p2.Item2);
                 newPopulation[i + 1] = new Specimen(p2.Item1 + p1.Item2);
             }
@@ -184,13 +173,12 @@ namespace AlgorytmGenetyczny
         {
             string path = "probes.txt";
             List<(double, double)> probes = new List<(double, double)>();
-            
+
             if (File.Exists(path))
             {
                 using StreamReader sr = new StreamReader(path);
                 string line;
                 while ((line = sr.ReadLine()) != null)
-                    
                 {
                     line = line.TrimStart();
                     string[] separated = line.Split("  ");
@@ -209,14 +197,14 @@ namespace AlgorytmGenetyczny
         static void geneticAlghoritm(int iterationCount, int specimenCount)
         {
             List<Specimen> population = initializePopulation(specimenCount, Specimen.chromLen);
-            List <(double, double)> probes = getProbes();
+            List<(double, double)> probes = getProbes();
             evaluatePopulation(population, probes);
             Specimen theBest = population.MinBy(s => s.rate);
             double populationAverage = Math.Round((double)population.Average(s => s.rate), 2);
             Console.WriteLine("Pierwsza populacja");
             showPopulation(population);
             Console.WriteLine($"Najlepszy z populacji:  {theBest.specimen} | {theBest.rate:F2}\nŚrednia populacji: {populationAverage}\n");
-            
+
             for (int i = 0; i < iterationCount; i++)
             {
                 List<Specimen> newPopulation = tournamentSelection(population);
@@ -232,12 +220,12 @@ namespace AlgorytmGenetyczny
                 population = newPopulation;
             }
         }
-        
+
         static void Main(string[] args)
         {
             int specimenCount = 13;
-            int iterationCount = 200;
-            Specimen.LBnP = 6;
+            int iterationCount = 100;
+            Specimen.LBnP = 4;
             Specimen.NumOfPar = 3;
             Specimen.minVal = 0;
             Specimen.maxVal = 3;
